9555af1e55bd4626f069808e6ad465372f429bbc
TerrenceS <terrence.shi@tvdms.com.hk>
2026-05-27 22:42:10 +0800

    inti with basic docker, env, package setup

---
+++ apps/backend/db/02_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO todos (title, description, is_completed) VALUES 
+('Buy groceries', 'Milk, Eggs, and Bread', false),
+('Prepare for interview', 'Review SQL syntax and Docker Compose', false),
+('Fix the leaking tap', 'Call the plumber', true);
