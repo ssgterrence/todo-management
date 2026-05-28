5163021bf9fd11fcd37157efc97abb721213aef1
TerrenceS <terrence.shi@tvdms.com.hk>
2026-05-28 12:52:09 +0800

    chore(docker): db table typo

--- apps/backend/src/db/01_init.sql
+++ apps/backend/src/db/01_init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE todos (
+CREATE TABLE duties (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
--- apps/backend/src/db/02_seed.sql
+++ apps/backend/src/db/02_seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO todos (title, description, is_completed) VALUES 
+INSERT INTO duties (title, description, is_completed) VALUES 
 ('Buy groceries', 'Milk, Eggs, and Bread', false),
 ('Prepare for interview', 'Review SQL syntax and Docker Compose', false),
 ('Fix the leaking tap', 'Call the plumber', true);
