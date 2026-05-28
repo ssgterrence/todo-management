74cd4b6fb0e0915fa908bb1301e35660fca9d429
TerrenceS <terrence.shi@tvdms.com.hk>
2026-05-28 12:54:29 +0800

    fix(port): change to 4001

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const main = async () => {
   const app = express();
-  const PORT = process.env["PORT"] || 3000;
+  const PORT = process.env["PORT"] || 4001;
   app.use(cors());
   app.use(express.json());
   const centralRoutes = await initCentralRoutes();
